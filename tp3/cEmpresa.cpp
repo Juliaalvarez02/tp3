c878a962a877ca7e0c3827161af2e9a0e9071e8f
Martina Martens <martensmartu@gmail.com>
2021-05-21 11:46:30 -0300

    modifique contrusctores y destructores

--- tp3/cEmpresa.cpp
+++ tp3/cEmpresa.cpp
@@ -2,17 +2,30 @@
 
 cEmpresa::cEmpresa()
 {
-	for (int i = 0; i < listaclientes->getCA(); i++) {
+	for (int i = 0; i < listaclientes->getTam(); i++) {
 		listaclientes[i] = NULL;
 	}
-	for (int i = 0; i < listavehiculos->getCA(); i++) {
+	for (int i = 0; i < listavehiculos->getTam(); i++) {
 		listavehiculos[i] = NULL;
 	}
 }
 
 cEmpresa::~cEmpresa()
 {
-	for (int i = 0; i < listaclientes->getCA(); i++) {
-		delete listaclientes;
+	if (listaclientes != NULL) {
+		for (int i = 0; i < listaclientes->getCA(); i++) {
+			if (&listaclientes[i] != NULL) {
+				delete& listaclientes[i];
+			}
+		}
+		delete[] listaclientes;
 	}
+		if (listavehiculos != NULL) {
+			for (int i = 0; i < listavehiculos->getCA(); i++) {
+				if (&listavehiculos[i] != NULL) {
+					delete& listavehiculos[i];
+				}
+			}
+			delete[] listavehiculos;
+		}
 }
